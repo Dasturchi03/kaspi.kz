d5f07c1dc915b6dacb622a5bedbe5676837600a3
Dasturchi5333 <118047053+Dasturchi5333@users.noreply.github.com>
2023-09-24 12:08:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ async def main():
     page = await brow.newPage()
     cookies = [{'name': 'layout', 'value': 'd', 'domain': 'kaspi.kz', 'path': '/shop', 'expires': 1726644470.633389, 'size': 7, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'k_stat', 'value': '74791a40-1e09-45f8-a9ed-d4ec9e7b7359', 'domain': 'kaspi.kz', 'path': '/', 'expires': 1726644470.844751, 'size': 42, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'ks.tg', 'value': '84', 'domain': 'kaspi.kz', 'path': '/', 'expires': 1726644470.928354, 'size': 7, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'kaspi.storefront.cookie.city', 'value': '632210000', 'domain': 'kaspi.kz', 'path': '/', 'expires': 1726644474, 'size': 37, 'httpOnly': False, 'secure': False, 'session': False}]
     await page.setCookie(*cookies)
+    await reset_json()
     for url in urls:
         # try:
             await page.goto(url)
@@ -141,6 +142,11 @@ async def get_data(text, url):
     
     with open('files/data.json', 'w', encoding='utf-8') as file:
         file.write(json.dumps(dc, indent=2, ensure_ascii=False))
+
+
+async def reset_json():
+    with open('files/data.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps({}))
 
 
 async def write_xlsx():
